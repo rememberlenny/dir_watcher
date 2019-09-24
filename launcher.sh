305d11230588f2d5751480fab321b831f0fafe8d
Leonard Bogdonoff <lkbgift@gmail.com>
2019-09-24 01:10:05 -0400

    Create launcher.sh

---
+++ launcher.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# launcher.sh
+# navigate to home directory, then to project directory, then execute python script, then back home
+
+cd ~/
+cd ~/Spaceship/publicart
+python dir_watcher/main.py
+cd ~/
