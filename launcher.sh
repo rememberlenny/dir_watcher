064bd3a773f7ef291f56d64d48ad5bf105b1cdb8
Leonard Bogdonoff <rememberlenny@gmail.com>
2019-09-24 07:09:12 +0100

    Save scraper updates

--- launcher.sh
+++ launcher.sh
@@ -2,7 +2,7 @@
 # launcher.sh
 # navigate to home directory, then to project directory, then execute python script, then back home
 
-cd ~/
-cd ~/Spaceship/publicart
+cd /home/pi/Spaceship/publicart/dir_watcher
+cd /home/pi/Spaceship/publicart
 python dir_watcher/main.py
-cd ~/
+wx
--- scraper.sh
+++ scraper.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # scraper.sh
 # kick off the instaloader task
-
+c
 cd ~/
 cd ~/Spaceship/publicart
-`instaloader --login streetarrrrt --no-videos --no-compress-json --geotags "#streetart"`
+instaloader --login streetarrrrt --no-videos --no-compress-json --geotags "#streetart"
