dec40b4c51d6d3110d2c21280a352f133797c423
Leonard Bogdonoff <rememberlenny@gmail.com>
2019-09-25 03:29:50 +0100

    Update the previous day script

--- index_previous_day_images.py
+++ index_previous_day_images.py
@@ -4,16 +4,16 @@ from itertools import dropwhile, takewhile
 import instaloader
 
 USERNAME = 'streetarrrrt'
-FILENAME = '../pwd/.instaloader-session'
+FILENAME = '/home/pi/Spaceship/publicart/pwd/.instaloader-session'
 
 L = instaloader.Instaloader(download_pictures=True, download_comments=False, download_videos=False, download_geotags=True, save_metadata=True, compress_json=False)
 L.load_session_from_file(USERNAME, FILENAME)
 
 posts = L.get_hashtag_posts('streetart')
 
-SINCE = datetime(2019, 9, 22)
+SINCE = datetime(2019, 9, 2)
 UNTIL = datetime(2019, 9, 23)
 
 for post in takewhile(lambda p: p.date > UNTIL, dropwhile(lambda p: p.date > SINCE, posts)):
     print(post.date_utc)
-    L.download_post(post, '#streetart/' + post.date_utc)
+    L.download_post(post, '#test-streetart/')
