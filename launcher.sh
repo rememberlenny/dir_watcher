b18f3acc872505236666d4c72bc8432fa1bdf914
Leonard Bogdonoff <rememberlenny@gmail.com>
2019-09-25 04:17:35 +0100

    Remove the launcher

--- launcher.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# launcher.sh
-# navigate to home directory, then to project directory, then execute python script, then back home
-
-cd /home/pi/Spaceship/publicart
-sleep 5
-echo 'Starting main...'
-sleep 5
-python /home/pi/Spaceship/publicart/dir_watcher/main.py
