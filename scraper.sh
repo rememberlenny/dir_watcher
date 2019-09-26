66312616ab9b7697a45380342aa142756991590a
Leonard Bogdonoff <rememberlenny@gmail.com>
2019-09-26 14:10:21 -0400

    Use shared vars

--- scraper.sh
+++ scraper.sh
@@ -2,14 +2,24 @@
 # scraper.sh
 # kick off the instaloader task
 
+export DEVICE_NAME='RPI-STREETART'
+export INSTAGRAM_USER_NAME='streetarrrrt'
+export INSTAGRAM_INDEX_HASHTAG='streetart'
+
 source /home/pi/instaloader/bin/activate 
+
 cd /home/pi/
 cd /home/pi/Spaceship/publicart/
+
 echo 'Starting in 20 seconds'
+
 sleep 10
 echo 'Starting in 10 seconds'
+
 sleep 5
 echo 'Starting in 5...'
+
 sleep 5
 echo 'Running instaloader'
-python /home/pi/Spaceship/publicart/dir_watcher/main.py & instaloader --login streetarrrrt --sessionfile /home/pi/Spaceship/publicart/pwd/.instaloader-session  --no-videos --no-compress-json --geotags "#streetart"
+
+python /home/pi/Spaceship/publicart/dir_watcher/main.py & sh -c 'instaloader --login ${0} --sessionfile /home/pi/Spaceship/publicart/pwd/.instaloader-session  --no-videos --no-compress-json --geotags "#${1}"' "$INSTAGRAM_USER_NAME" "$INSTAGRAM_INDEX_HASHTAG"
